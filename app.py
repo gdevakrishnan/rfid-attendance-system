02b73fef43fb5b934e5d7f0f9b6ab5d9afd97ad7
gdevakrishnan <gdevakrishnan700@gmail.com>
2024-06-19 14:17:28 +0530

    Schedule to calculate tokens of an employee

--- app.py
+++ app.py
@@ -54,6 +54,8 @@ def get_attendance_data():
     # Return the data as JSON
     return jsonify(attendance_data)
 
+##############################################################################
+
 # To add a new Employee
 @app.route('/add_worker', methods=['POST'])
 def add_worker():
@@ -90,6 +92,8 @@ def add_worker():
         print(f"Error add worker: {e}")
         return f"Error: {e}", 500  # Return error message with HTTP status code 500
 
+##############################################################################
+    
 # To find the worker by rfid
 @app.route('/find_worker', methods=['POST'])
 def find_worker():
@@ -109,6 +113,8 @@ def find_worker():
         return f"Error: {e}", 500  # Return error message with HTTP status code 500
 
 
+##############################################################################
+    
 # To find the worker by rfid and put attendance
 @app.route('/attendance', methods=['POST'])
 def attendance():
@@ -122,6 +128,8 @@ def attendance():
     except Exception as e:
         print(f"Error data retrieved and attendance: {e}")
         return f"Error: {e}", 500  # Return error message with HTTP status code 500
+
+##############################################################################
 
 # To put attendance to DB
 def put_attendance(data):
@@ -159,6 +167,8 @@ def put_attendance(data):
         except TwilioRestException as e:
             print(f"Failed to send message: {e}")
 
+##############################################################################
+
 # To find the attendance is first time of the day
 def firstTimeAttendance(name, rfid_id, date):
     cursor = list(attendanceCollection.find({"name": name, "rfid_id": rfid_id, "date": date}))
@@ -167,7 +177,9 @@ def firstTimeAttendance(name, rfid_id, date):
     else:
         return True
 
-# TO find the worker is in or out of work
+##############################################################################
+
+# To find the worker is in or out of work
 def presenceOfWorker(name, rfid_id, date):
     cursor = list(attendanceCollection.find({"rfid_id": rfid_id, "name": name, "date": date}))        
     if (len(cursor) > 0):
@@ -176,64 +188,9 @@ def presenceOfWorker(name, rfid_id, date):
                 return (not (element['presence']))
     else:
         return True
+##############################################################################
 
-def send_worker_profiles():
-    # To check to day is a holiday or not
-    current_datetime = datetime.now()
-    today_date = current_datetime.date()
-    weekday = today_date.weekday()  # Monday is 0, Sunday is 6
-    isHolidayToday = {'status': 'Not a Holiday', "isHoliday": False}
-
-    if weekday >= 5:  # 5 and 6 represent Saturday and Sunday respectively
-        isHolidayToday = {'status': 'Weekend', "isHoliday": True}
-
-    for holiday in holidays_data:
-        if holiday['date'] == today_date:
-            isHolidayToday = {'status': 'Holiday', 'holiday_title': holiday['holiday_title'], "isHoliday": True}
-
-    if (not (isHolidayToday['isHoliday'])):
-        # Fetch all workers from the collection
-        workers = workersCollection.find({})
-
-        for worker in workers:
-            # Extract mobile number and details from worker
-            mobile = worker.get('mobile')
-            name = worker.get('name')
-            age = worker.get('age')
-            role = worker.get('role')
-            working_hours = worker.get('working_hours')
-            salary = worker.get('salary')
-            token = worker.get('token')
-
-            # Construct the message
-            message_body = (
-                f"Profile for {name}:\n"
-                f"Name: {name}\n"
-                f"Age: {age}\n"
-                f"Role: {role}\n"
-                f"Working Hours: {working_hours}\n"
-                f"Salary: {salary}\n"
-                f"Token: {token}"
-            )
-
-            if mobile:
-                try:
-                    # Send the message using Twilio
-                    message = messenger_client.messages.create(
-                        from_=twilio_mobile_number,
-                        body=message_body,
-                        to="+91" + str(mobile)
-                    )
-                    print(f"Message sent successfully to {name}: SID {message.sid}")
-                except TwilioRestException as e:
-                    print(f"Failed to send message to {name}: {e}")
-
-# Schedule the task to run daily at 6 PM to send the daily reports
-scheduler = BackgroundScheduler()
-trigger = CronTrigger(hour=18, minute=0)  # 6 PM
-scheduler.add_job(send_worker_profiles, trigger)
-scheduler.start()
-
+# To send message to an employess about holidays at 4am
 def send_holiday_message_to_worker():
     # To check to day is a holiday or not
     current_datetime = datetime.now()
@@ -293,10 +250,82 @@ def send_holiday_message_to_worker():
 
 # Schedule to send the message at 4am. To inform today office is a holiday or not
 scheduler = BackgroundScheduler()
-trigger = CronTrigger(hour=4, minute=0)  # 4 AM
+trigger = CronTrigger(hour=4, minute=0)  # 4AM
 scheduler.add_job(send_holiday_message_to_worker, trigger)
 scheduler.start()
 
+##############################################################################
+
+# Schedule to update the token of an employee at 6pm
+def calculate_token():
+    print("Calculate Token")
+
+scheduler = BackgroundScheduler()
+trigger = CronTrigger(hour=4, minute=0)  # 4 AM
+scheduler.add_job(calculate_token, trigger)
+scheduler.start()
+
+##############################################################################
+
+# To send daily reports to an appropriate employee at 7pm
+def send_worker_profiles():
+    # To check to day is a holiday or not
+    current_datetime = datetime.now()
+    today_date = current_datetime.date()
+    weekday = today_date.weekday()  # Monday is 0, Sunday is 6
+    isHolidayToday = {'status': 'Not a Holiday', "isHoliday": False}
+
+    if weekday >= 5:  # 5 and 6 represent Saturday and Sunday respectively
+        isHolidayToday = {'status': 'Weekend', "isHoliday": True}
+
+    for holiday in holidays_data:
+        if holiday['date'] == today_date:
+            isHolidayToday = {'status': 'Holiday', 'holiday_title': holiday['holiday_title'], "isHoliday": True}
+
+    if (not (isHolidayToday['isHoliday'])):
+        # Fetch all workers from the collection
+        workers = workersCollection.find({})
+
+        for worker in workers:
+            # Extract mobile number and details from worker
+            mobile = worker.get('mobile')
+            name = worker.get('name')
+            age = worker.get('age')
+            role = worker.get('role')
+            working_hours = worker.get('working_hours')
+            salary = worker.get('salary')
+            token = worker.get('token')
+
+            # Construct the message
+            message_body = (
+                f"Profile for {name}:\n"
+                f"Name: {name}\n"
+                f"Age: {age}\n"
+                f"Role: {role}\n"
+                f"Working Hours: {working_hours}\n"
+                f"Salary: {salary}\n"
+                f"Token: {token}"
+            )
+
+            if mobile:
+                try:
+                    # Send the message using Twilio
+                    message = messenger_client.messages.create(
+                        from_=twilio_mobile_number,
+                        body=message_body,
+                        to="+91" + str(mobile)
+                    )
+                    print(f"Message sent successfully to {name}: SID {message.sid}")
+                except TwilioRestException as e:
+                    print(f"Failed to send message to {name}: {e}")
+
+# Schedule the task to run daily at 6 PM to send the daily reports
+scheduler = BackgroundScheduler()
+trigger = CronTrigger(hour=19, minute=0)  # 7 PM
+scheduler.add_job(send_worker_profiles, trigger)
+scheduler.start()
+
+##############################################################################
 
 if __name__ == '__main__':
     app.run(debug=True)
